1f76453918540347860a5e4b69d157d020f5eb6d
leicaflorian <florian.leica@gmail.com>
2022-04-23 10:43:51 +0200

    Updates api notifications controller

--- app/Http/Controllers/Api/NotificationController.php
+++ app/Http/Controllers/Api/NotificationController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Events\NotificationCreated;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ReadNotificationRequest;
 use App\Http\Requests\StoreNotificationRequest;
+use App\Jobs\SendEmail;
+use App\Models\Job;
 use App\Models\Notification;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\DB;
 use Jenssegers\Mongodb\Eloquent\Model;
 
 class NotificationController extends Controller {
+  
   /**
    * Display a listing of the resource.
    *
@@ -19,52 +26,152 @@ class NotificationController extends Controller {
    * @return JsonResponse
    */
   public function index(Request $request): JsonResponse {
-    $user     = $request->user()->_id;
+    /**
+     * @var User $user
+     */
+    $user     = $request->user();
     $platform = $request->input("platform");
-  
-    $query = [
-      // User is a receiver
-      "receivers._id"   => $user,
-      // User is not in readings
-      "readings.userId" => ["\$ne" => $user],
-    ];
-  
-    if ($platform) {
-      $query["platforms"] = ["\$eq" => $platform];
+    $app      = $request->input("app");
+    $showRead = $request->input("read", false);
+    
+    if ($showRead) {
+      $notifications = $user->readNotificationsByPlatform($platform, $app, true);
+    } else {
+      $notifications = $user->unreadNotificationsByPlatform($platform, $app, true);
     }
-  
-    // For some reason, using classic where doesn't work on subArray element so must use "whereRaw" instead
-    $notifications = Notification::whereRaw($query)->orderBy("created_id", "desc")->get();
-  
+    
     return response()->json($notifications);
   }
   
   /**
-   * Store a newly created resource in storage.
+   * Display a listing of the resource.
    *
-   * @param  StoreNotificationRequest  $request
+   * @param  Request  $request
    *
-   * @return Model
+   * @return JsonResponse
    */
-  public function store(StoreNotificationRequest $request): Model {
-    $data = $request->validated();
-  
-    return Notification::create($data);
+  public function counters(Request $request): JsonResponse {
+    /**
+     * @var User $user
+     */
+    $user     = $request->user();
+    $platform = $request->input("platform");
+    $app      = $request->input("app");
+    
+    $result = [
+      "unread" => $user->unreadNotificationsByPlatform($platform, $app, false, true),
+      "read"   => $user->readNotificationsByPlatform($platform, $app, false, true)
+    ];
+    
+    return response()->json($result);
   }
   
   /**
    * Update the specified resource in storage.
    *
    * @param  ReadNotificationRequest  $request
-   * @param  Notification             $notification
+   * @param  string                   $notification
    *
-   * @return JsonResponse
+   * @return Response
    */
-  public function read(ReadNotificationRequest $request, Notification $notification): JsonResponse {
+  public function read(ReadNotificationRequest $request, $notificationId): Response {
     $platform = $request->query("platform");
-  
-    $notification->setAsRead($request->user(), $platform);
-  
-    return response()->json($notification);
+    $app      = $request->input("app");
+    
+    /**
+     * @var $user User
+     */
+    $user              = $request->user();
+    $notifications     = $user->unreadNotificationsByPlatform($platform, $app);
+    $foundNotification = false;
+    
+    foreach ($notifications as $notification) {
+      if ($notification->id == $notificationId) {
+        $notification->markAsRead($platform);
+        $foundNotification = true;
+      }
+    }
+    
+    return response($foundNotification ? "OK" : "NOT_FOUND_OR_ALREADY_READ");
   }
+  
+  /**
+   * Update the specified resource in storage based on the extraData prop multiReadBy
+   *
+   * @param  ReadNotificationRequest  $request
+   * @param  string                   $multiReadBy
+   *
+   * @return Response
+   */
+  public function readByContent(ReadNotificationRequest $request, $multiReadBy): Response {
+    $platform = $request->query("platform");
+    $app      = $request->input("app");
+    
+    /**
+     * @var $user User
+     */
+    $user              = $request->user();
+    $notifications     = $user->unreadNotificationsByPlatform($platform, $app);
+    $foundNotification = false;
+    
+    
+    foreach ($notifications as $notification) {
+      $extraData = $notification->data["extraData"];
+      
+      if ($extraData && key_exists("multiReadBy", $extraData) && $extraData["multiReadBy"] == $multiReadBy) {
+        $notification->markAsRead($platform);
+        $foundNotification = true;
+      }
+    }
+    
+    return response($foundNotification ? "OK" : "NOT_FOUND_OR_ALREADY_READ");
+  }
+  
+  public function readAll(ReadNotificationRequest $request): JsonResponse {
+    $platform = $request->query("platform");
+    $app      = $request->input("app");
+    
+    /**
+     * @var $user User
+     */
+    $user = $request->user();
+    
+    $notifications = $user->unreadNotificationsByPlatform($platform, $app);
+    
+    foreach ($notifications as $notification) {
+      $notification->markAsRead($platform);
+    }
+    
+    return response()->json("OK");
+  }
+  
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  ReadNotificationRequest  $request
+   * @param  string                   $notification
+   *
+   * @return Response
+   */
+  public function unread(ReadNotificationRequest $request, $notificationId): Response {
+    $platform = $request->query("platform");
+    $app      = $request->input("app");
+    
+    /**
+     * @var $user User
+     */
+    $user              = $request->user();
+    $notifications     = $user->readNotificationsByPlatform($platform, $app);
+    $foundNotification = false;
+    
+    foreach ($notifications as $notification) {
+      if ($notification->id == $notificationId) {
+        $notification->markAsUnread($platform);
+        $foundNotification = true;
+      }
+    }
+    
+    return response($foundNotification ? "OK" : "NOT_FOUND_OR_ALREADY_READ");
+  }
+  
 }
