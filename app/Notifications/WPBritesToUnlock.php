fb85e33cf1d4d1bb3eb4e9432fa892cbf5c56422
leicaflorian <florian.leica@gmail.com>
2022-10-11 19:33:05 +0200

    Adds WPBritesToUnlock notification

---
+++ app/Notifications/WPBritesToUnlock.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Traits\BasicNotification;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class WPBritesToUnlock extends Notification {
+  use Queueable, BasicNotification;
+}
