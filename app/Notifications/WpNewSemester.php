be1fabf237d6a22f80ffcde6124c16c8dc293930
leicaflorian <florian.leica@gmail.com>
2023-01-13 14:53:25 +0100

    Fixes class case

--- app/Notifications/WpNewSemester.php
+++ app/Notifications/WpNewSemester.php
@@ -6,6 +6,6 @@ use App\Traits\BasicNotification;
 use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 
-class WPNewSemester extends Notification {
+class WpNewSemester extends Notification {
   use Queueable, BasicNotification;
 }
